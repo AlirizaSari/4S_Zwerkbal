7e56db3d37c9ca7c24193c12d1485bf5c3a16372
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-09-26 15:13:29 +0200

    a

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,2 +1,31 @@
+@extends('layout')
 
+@section('nav')
+    <nav>
+        <a href="/">Home</a>
+        <a href="/tournaments" class="active">Toernooien</a>
+        <a href="/teams">Teams</a>
+        <a href="/players">Spelers</a>
+    </nav>
+@endsection
+
+@section('content')
+
+    <h1>Teams</h1>
+    <table class="table">
+        <tr>
+            <th>Team</th>
+            <th>Soort</th>
+            <th>Herkomst</th>
+        </tr>
+        @foreach($teams as $team)
+            <tr>
+                <td>{{ $team->name }}</td>
+                <td>{{ $team->type }}</td>
+                <td>{{ $team->origin }}</td>
+            </tr>
+        @endforeach
+    </table>
+    
+@endsection
 <!-- hier komt een lijst van teams -->
